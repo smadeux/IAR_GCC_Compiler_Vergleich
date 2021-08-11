3d98ff41da827077b327e7c8c10688f988b62643
Scott Madeux <scott.madeux@gmail.com>
2021-08-11 11:30:37 +0200

    Komplett Projekt Dateien hinzugefügt

---
+++ Compiler Haupttests STM32H743ZI/Core/Inc/testfunktionen.h
@@ -0,0 +1,10 @@
+#ifndef TEST_FUNCTIONEN_H
+#define TEST_FUNCTIONEN_H
+
+int* Test_Matrix_Dot_Produkt();
+char Test_Switch_Case_Seq(int num);
+char Test_Switch_Case_Nicht_Seq(int num);
+int Test_Dividier_Zufaellige_Int(int i);
+float Test_Dividier_Zufaellige_Float(int i);
+
+#endif
